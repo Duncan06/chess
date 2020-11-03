f51dc4a59d273c7f8e3910015cbe26df81368d7b
Rumsauce <tylerdownie0@gmail.com>
2020-11-03 12:46:30 -0800

    add player move, player select, and check move type

--- lib/chess.rb
+++ lib/chess.rb
@@ -8,7 +8,7 @@ module Chess
 
     class Board
 
-        attr_accessor :board
+        attr_accessor :board, :spots
 
         def initialize
 
@@ -29,6 +29,26 @@ module Chess
                         [0, 1] => ["White Pawn", "\u2659"], [1, 1] => ["White Pawn", "\u2659"], [2, 1] => ["White Pawn", "\u2659"], [3, 1] => ["White Pawn", "\u2659"], [4, 1] => ["White Pawn", "\u2659"], [5, 1] => ["White Pawn", "\u2659"], [6, 1] => ["White Pawn", "\u2659"], [7, 1] => ["White Pawn", "\u2659"],
 
                         [0, 0] => ["White Rook", "\u2656"], [1, 0] => ["White Knight", "\u2658"], [2, 0] => ["White Bishop", "\u2657"], [3, 0] => ["White Queen", "\u2655"], [4, 0] => ["White King", "\u2654"], [5, 0] => ["White Bishop", "\u2657"], [6, 0] => ["White Knight", "\u2658"], [7, 0] => ["White Rook", "\u2656"]
+
+                    }
+
+            @spots = { 
+                    
+                        "a1" => [0,0], "a2" => [1,0], "a3" => [2, 0], "a4" => [3, 0], "a5" => [4, 0], "a6" => [5, 0], "a7" => [6, 0],  "a8" => [7, 0],
+
+                        "b1" => [0, 1], "b2" => [1, 1],  "b3" => [2, 1],  "b4" => [3, 1], "b5" => [4, 1], "b6" => [5, 1],  "b7" => [6, 1], "b8" => [7, 1],
+
+                        "c1" => [0, 2], "c2" => [1, 2], "c3" => [2, 2], "c4" => [3, 2], "c5" => [4, 2], "c6" => [5, 2], "c7" => [6, 2],"c8" => [7, 2],
+                        
+                        "d1" => [0, 3], "d2" => [1, 3], "d3" => [2, 3], "d4" => [3, 3], "d5" => [4, 3], "d6" => [5, 3], "d7" => [6, 3], "d8" => [7, 3],
+
+                        "e1" => [0, 4],  "e2" => [1, 4],  "e3" => [2, 4],  "e4" => [3, 4], "e5" => [4, 4], "e6" => [5, 4], "e7" => [6, 4], "e8" => [7, 4],
+
+                        "f1" => [0, 5], "f2" => [1, 5],  "f3" => [2, 5], "f4" => [3, 5],  "f5" => [4, 5], "f6" => [5, 5], "f7" => [6, 5], "f8" => [7, 5],
+
+                        "g1" => [0, 6], "g2" => [1, 6], "g3" => [2, 6],  "g4" => [3, 6], "g5" => [4, 6], "g6" => [5, 6], "g7" => [6, 6], "g8" => [7, 6],
+
+                        "h1" => [0, 7], "h2" => [1, 7], "h3" => [2, 7], "h4" => [3, 7], "h5" => [4, 7], "h6" => [5, 7], "h7" => [6, 7], "h8" => [7, 7]
 
                     }
 
@@ -90,6 +110,94 @@ module Chess
 
             check_capture_or_move(start, last, turn, valid)
 
+        end
+
+        def player_select()
+
+            answer = gets.chomp
+
+            answer = answer.downcase
+
+            if @spots.has_key?(answer)
+                
+                @spots[answer]
+
+            else
+
+                puts "Please provide correct input."
+
+                player_select()
+
+            end
+
+        end
+
+        def player_move(turn)
+
+            current_player = turn % 2
+
+            current_player == 0 ? "White" : "Black"
+
+            puts "It is your turn #{current_player}"
+
+            puts "Please enter a square to select in the format C4"
+
+            start = player_select()
+
+            puts "where would you like to move to?"
+
+            last = player_select()
+
+            if check_move_type(start, last, turn)
+
+                capture_piece?(start, last, turn)
+
+            else
+
+                "This piece cannot move there."
+
+                player_move()
+
+            end
+
+        end
+
+        def check_move_type(start, last, turn)
+
+            name = @board[start][0]
+
+            case name
+
+            when "White Knight" || "Black Knight"
+
+                knight_moves(start, last, turn)
+
+            when "White Bishop" || "Black Bishop"
+
+                bishop_moves(start, last, turn)
+
+            when "White Rook" || "Black Rook"
+
+                rook_moves(start, last, trun)
+
+            when "White King" || "Black King"
+
+                king_moves(start, last, turn)
+
+            when "White Queen" || "Black Queen" 
+
+                queen_moves(start, last, turn)
+
+            when "White Pawn" || "Black Pawn"
+
+                pawn_moves(start, last, turn)
+
+            else
+
+                false
+
+            end
+        
         end
 
         def capture_piece?(start, last, turn)
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -377,6 +377,112 @@ describe Chess do
 
         end
 
+        describe "#player_select" do
+
+            it "takes player select and returns selected square coordinates" do
+
+                io = StringIO.new
+
+                io.puts "a1"
+
+                io.rewind
+
+                $stdin = io
+
+                game = Chess::Board.new
+
+                result = game.player_select()
+
+                gets
+
+                expect(result).to eq([0, 0])
+                
+                $stdin = STDIN
+
+            end
+
+            it "sanitizes input" do
+
+                io = StringIO.new
+
+                io.puts "U2"
+
+                io.puts "b1"
+
+                io.rewind
+
+                $stdin = io
+
+                game = Chess::Board.new
+                
+                result = game.player_select()
+
+                gets
+
+                expect { print "Please provide correct input." }.to output.to_stdout
+
+                gets
+
+                expect(result).to eq([0, 1])
+
+                $stdin = STDIN
+
+            end
+
+        end
+
+        describe "#player_move" do
+
+            it "takes player move and correctly moves piece" do
+
+                io = StringIO.new
+
+                io.puts "b2"
+
+                io.puts "c2"
+
+                io.rewind
+
+                $stdin = io
+
+                game = Chess::Board.new
+
+                result = game.player_move(0)
+
+                expect { print "It is your turn White"}.to output.to_stdout
+
+                expect { print "Please enter a square to select in the format C4" }.to output.to_stdout
+
+                gets
+
+                gets
+
+                expect(game.board).to eq({
+                    
+                    [0, 7] => ["Black Rook", "\u265C"], [1, 7] => ["Black Knight", "\u265E"], [2, 7] => ["Black Bishop", "\u265D"], [3, 7] => ["Black King", "\u265A"], [4, 7] => ["Black Queen", "\u265B"], [5, 7] => ["Black Bishop", "\u265D"], [6, 7] => ["Black Knight", "\u265E"], [7, 7] => ["Black Rook", "\u265C"],
+
+                    [0, 6] => ["Black Pawn", "\u265F"], [1, 6] => ["Black Pawn", "\u265F"], [2, 6] => ["Black Pawn", "\u265F"], [3, 6] => ["Black Pawn", "\u265F"], [4, 6] => ["Black Pawn", "\u265F"], [5, 6] => ["Black Pawn", "\u265F"], [6, 6] => ["Black Pawn", "\u265F"], [7, 6] => ["Black Pawn", "\u265F"],
+
+                    [0, 5] => nil, [1,5] => nil, [2, 5] => nil, [3, 5] => nil, [4, 5] => nil, [5, 5] => nil, [6, 5] => nil, [7, 5] => nil,
+
+                    [0, 4] => nil, [1,4] => nil, [2, 4] => nil, [3, 4] => nil, [4, 4] => nil, [5, 4] => nil, [6, 4] => nil, [7, 4] => nil,
+
+                    [0, 3] => nil, [1,3] => nil, [2, 3] => nil, [3, 3] => nil, [4, 3] => nil, [5, 3] => nil, [6, 3] => nil, [7, 3] => nil,
+
+                    [0, 2] => nil, [1,2] => ["White Pawn", "\u2659"], [2, 2] => nil, [3, 2] => nil, [4, 2] => nil, [5, 2] => nil, [6, 2] => nil, [7, 2] => nil,
+
+                    [0, 1] => ["White Pawn", "\u2659"], [1, 1] => nil, [2, 1] => ["White Pawn", "\u2659"], [3, 1] => ["White Pawn", "\u2659"], [4, 1] => ["White Pawn", "\u2659"], [5, 1] => ["White Pawn", "\u2659"], [6, 1] => ["White Pawn", "\u2659"], [7, 1] => ["White Pawn", "\u2659"],
+
+                    [0, 0] => ["White Rook", "\u2656"], [1, 0] => ["White Knight", "\u2658"], [2, 0] => ["White Bishop", "\u2657"], [3, 0] => ["White Queen", "\u2655"], [4, 0] => ["White King", "\u2654"], [5, 0] => ["White Bishop", "\u2657"], [6, 0] => ["White Knight", "\u2658"], [7, 0] => ["White Rook", "\u2656"]
+
+                })
+
+                $stdin = STDIN
+
+            end
+
+        end
+
         describe "#display_board" do
 
             it "shows game board" do
